278641b2efd195eabf160f5fe15de792fd5fdf04
yewon97 <dpdnjs9936@gmail.com>
2021-12-06 23:01:48 +0900

    이벤트 핸들러 추가, js 파일 추가

--- join.js
+++ join.js
@@ -5,15 +5,58 @@ function form_check() { // form요소 submit 전 유효성 검사
   let repwd = document.getElementById("repwd");
   let mobile = document.getElementById("mobile");
   let agree = document.getElementById("agree");
+  
+  function red_err_txt() { 
+    err_txt.style.color = "red";
+  }
 
-  if(u_name.value == "") {
-    let err_txt = document.querySelector(".err_name");
-    err_txt.innerHTML = "* 이름을 입력하세요."
+  if (u_name.value == "") {
+    var err_txt = document.querySelector(".err_name");
+    red_err_txt();
+    err_txt.textContent = "* 이름을 입력하세요.";
+    u_name.focus();
+    return false;
+  }
+  if (u_id.value == "") {
+    var err_txt = document.querySelector(".err_id");
+    red_err_txt();
+    err_txt.textContent = "* 아이디를 입력하세요";
+    u_id.focus();
+    return false;
+  }
+  const uid_len = u_id.value.length;
+  if (uid_len < 4 || uid_len > 12) {
+    var err_txt = document.querySelector(".err_id");
+    red_err_txt();
+    err_txt.textContent = "* 4~12글자까지만 입력 가능합니다.";
+    u_id.focus();
+    return false;
+  }
+  if (pwd.value == "") {
+    var err_txt = document.querySelector(".err_pwd");
+    red_err_txt();
+    err_txt.textContent = "* 비밀번호를 입력하세요";
+    pwd.focus();
+    return false;
+  }
+  var pwd_len = pwd.value.length;
+  if (pwd_len < 4 || pwd_len > 12) {
+    var err_txt = document.querySelector(".err_pwd");
+    red_err_txt();
+    err_txt.textContent = "* 4~12글자까지만 입력 가능합니다.";
+    pwd.focus();
+    return false;
+  }
+  if (pwd.value != repwd.value) {
+    var err_txt = document.querySelector(".err_repwd");
+    red_err_txt();
+    err_txt.textContent = "* 비밀번호를 확인해주세요.";
+    repwd.focus();
+    return false;
+  }
+  if (!agree.checked) {
+    alert("약관 동의가 필요합니다.");
+    agree.focus();
+    return false;
   }
 }
-
-let submit_join_form = document.querySelector("#join_form");
-submit_join_form.addEventListener('submit', (e) => {
-  form_check();
-  e.preventDefault();
-});
